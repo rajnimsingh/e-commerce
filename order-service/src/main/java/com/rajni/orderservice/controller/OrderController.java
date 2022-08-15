be5d95a0147cb8967608f2fa3e97179427c82baf
rajnimsingh <rajni.manjitsingh@gmail.com>
2022-08-15 14:39:49 +0530

    Added Counter metrics to placeorder

--- order-service/src/main/java/com/rajni/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/rajni/orderservice/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.rajni.ecommers.common.model.response.OrderResponse;
 import com.rajni.ecommers.common.model.Status;
 import com.rajni.orderservice.model.OrderException;
 import com.rajni.orderservice.service.OrderService;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,9 +23,12 @@ public class OrderController {
     private static final Logger LOG = LoggerFactory.getLogger(OrderController.class.getName());
     private OrderService orderService;
 
+    private MeterRegistry meterRegistry;
+
     @PostMapping("/place-order")
     public OrderResponse placeOrder(@RequestBody OrderRequest orderRequest) {
         try {
+            meterRegistry.counter("place-order").increment();
             LOG.info("place Order execution starts.");
             LOG.info("Order placed for the item Id, {} by the user {}", orderRequest.getItemId(), orderRequest.getUser().getUserId());
             return OrderResponse.builder()
